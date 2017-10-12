1f9142bb6854165daf477e9253dd4e9159b41e79
Jaskaranbir <jaskaranbir_singh@ymail.com>
2017-10-12 01:23:43 -0400

    Improved stdout for deployment-counter status

--- scripts/deploy_wait_message.sh
+++ scripts/deploy_wait_message.sh
@@ -8,34 +8,43 @@ function get_deploy_status() {
   res=$(curl --write-out %{http_code} --silent --output /dev/null localhost:8080/agario)
 }
 
-tries=15
+tries=20
 get_deploy_status
 while (( res != 200 && res != 302 && --tries != 0))
 do
-  echo -n ". "
-  sleep 1
+  echo "==> Waiting for project deployment pipeline (attempt: $((20 - $tries)) of 20)."
+  sleep 2
   get_deploy_status
 done
 
-echo -e ".\n"
+echo
 
 if (( tries == 0 )); then
   for cntnr in $(docker ps -q)
   do
-    echo "========================================"
-    echo "Printing logs for: $cntnr"
+    cntnrInfo=$(docker ps -f "id=$cntnr" --format "---Container: "{{.Names}}\t" and ---Image: "{{.Image}})
+
+    echo "----------------------------------------"
+    echo -e "Printing logs for $cntnrInfo \n\n"
+    echo "----------------------------------------"
+
     docker logs $cntnr
     echo -e "\n\n\n\n"
   done
 
+  # Panic messages
+  echo "=============================================================="
   echo "Timeout for deployment pipeline. Most likely an error occured."
   echo -e "Please provide me with above printed logs so I can look into issue.\n"
   echo "For self troubleshooting, follow steps below."
+
   echo -e "Please run 'vagrant ssh' and check docker logs for troubleshooting.\n"
   echo -e "Alternatively, you can also try running docker containers manually after vagrant ssh using:\n"
+
   echo "docker-compose up"
   echo "or"
   echo -e "docker-compose up -d\n"
+
   echo "NOTE: Docker and source files are mounted at '/vagrant'. So you will need to cd into that first."
 else
   echo "---------------------------------------------------------"
